3a34724d6dbf325ada65311464f274a26297e7a0
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-10-18 20:57:38 +0200

    tasks 1, 2,3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,16 @@
+class Key {}
+class MyHouse extends Key {
+  constructor();
+  super();
+  openDoor();
+  comeIn();
+}
+class Person extends Key {
+  constructor();
+  super();
+  getKey();
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +19,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
